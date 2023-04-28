6c8213c07823f2718672c1c36aa5bfd14e278f9e
osmangony01 <mdosmangoni376@gmail.com>
2023-04-28 23:00:54 +0600

    update some logic

--- index.js
+++ index.js
@@ -16,8 +16,26 @@ app.get("/categories", (req, res) => {
     res.send(categories);
 })
 
-app.get("/new", (req, res) => {
+app.get("/category/:id", (req, res) => {
+    const id = req.params.id;
+    if(id == 0){
+        res.send(news);
+    }
+    else{
+        const selectedCategory = news.filter(n => n.category_id === id);
+        res.send(selectedCategory);
+    }
+    
+})
+
+app.get("/news", (req, res) => {
     res.send(news);
+})
+
+app.get("/news/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 })
 
 app.listen(port, () => {
